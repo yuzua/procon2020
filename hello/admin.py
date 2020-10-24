effb936aab1bd6f10b3f5796f7b958753cce578e
齋藤啓太 <saitoukeita@yuzukinoMBP.AirPort>
2020-10-24 23:21:44 +0900

    2020/10/24

---
+++ hello/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Test1
+# Register your models here.
+
+admin.site.register(Test1)
---
+++ hello/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Test1(models.Model):
+    name = models.CharField(max_length=100)
